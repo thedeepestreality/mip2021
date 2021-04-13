3c7e3fd3626ee3371b447f3781e9edfa194efe32
Ruslan Sevostyanov <zetoster@gmail.com>
2021-04-13 12:46:26 +0300

    some useful tools

---
+++ useful_tools/dummy.m
@@ -0,0 +1,9 @@
+A = diag([1, 2]);
+d = det(A);
+
+z = test()
+
+function x = test()
+x = 2;
+end
+
---
+++ useful_tools/test_func.m
@@ -0,0 +1,13 @@
+function [ret_val,ret_val2] = test_func(param)
+
+ret_val = local_f();
+
+ret_val2 = 3;
+
+end
+
+function x = local_f()
+
+x = 0;
+
+end
